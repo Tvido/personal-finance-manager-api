71c0922f3d638a4471c293f8894d3f7d9e0f8475
Volodymyr Viter <mad.tvido@gmail.com>
2021-11-09 13:07:03 +0200

    Update add.js

--- controllers/transactions/add.js
+++ controllers/transactions/add.js
@@ -1,42 +1,42 @@
-const { Transactions } = require('../../models/transactions');
-
-const add = async (req, res, next) => {
-  const result = await Transactions.create(req.body);
-  res.status(201).json({ result });
-
-  // try {
-  //   const { error } = Transactions.validate(req.body);
-  //   if (error) {
-  //     return res.status(400).json({
-  //       message: 'Missing required name field',
-  //     });
-  //   }
-
-  //   const newTransaction = await Transactions.add(req.body);
-  //   res.status(201).json({ newTransaction });
-  // } catch (error) {
-  //   next(error);
-  // }
-};
-
-// const transactionsOperations = require("../../model/transactions");
-
-// const { joiTransactionSchema } = require("../../validation");
+// const { Transactions } = require('../../models/transactions');
 
 // const add = async (req, res, next) => {
-//   try {
-//     const { error } = joiTransactionSchema.validate(req.body);
-//     if (error) {
-//       return res.status(400).json({
-//         message: "Missing required name field",
-//       });
-//     }
+//   const result = await Transactions.create(req.body);
+//   res.status(201).json({ result });
 
-//     const newTransaction = await transactionsOperations.add(req.body);
-//     res.status(201).json({ newTransaction });
-//   } catch (error) {
-//     next(error);
+// try {
+//   const { error } = Transactions.validate(req.body);
+//   if (error) {
+//     return res.status(400).json({
+//       message: 'Missing required name field',
+//     });
 //   }
+
+//   const newTransaction = await Transactions.add(req.body);
+//   res.status(201).json({ newTransaction });
+// } catch (error) {
+//   next(error);
+// }
 // };
+
+const Transactions = require('../../model/transactions');
+
+const { joiTransactionSchema } = require('../../validation');
+
+const add = async (req, res, next) => {
+  try {
+    const { error } = joiTransactionSchema.validate(req.body);
+    if (error) {
+      return res.status(400).json({
+        message: 'Missing required name field',
+      });
+    }
+
+    const newTransaction = await Transactions.add(req.body);
+    res.status(201).json({ newTransaction });
+  } catch (error) {
+    next(error);
+  }
+};
 
 module.exports = add;
