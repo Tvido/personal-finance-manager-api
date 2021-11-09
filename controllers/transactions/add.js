75e22ffac206fc18bd46cbd56f6fb89f65b40ca8
Volodymyr Viter <mad.tvido@gmail.com>
2021-11-09 13:24:17 +0200

    Update add.js

--- controllers/transactions/add.js
+++ controllers/transactions/add.js
@@ -1,42 +1,28 @@
-// const { Transactions } = require('../../models/transactions');
-
-// const add = async (req, res, next) => {
-//   const result = await Transactions.create(req.body);
-//   res.status(201).json({ result });
-
-// try {
-//   const { error } = Transactions.validate(req.body);
-//   if (error) {
-//     return res.status(400).json({
-//       message: 'Missing required name field',
-//     });
-//   }
-
-//   const newTransaction = await Transactions.add(req.body);
-//   res.status(201).json({ newTransaction });
-// } catch (error) {
-//   next(error);
-// }
-// };
-
-const Transactions = require('../../model/transactions');
-
-const { joiTransactionSchema } = require('../../validation');
+const { Transactions } = require('../../models/transactions');
 
 const add = async (req, res, next) => {
-  try {
-    const { error } = joiTransactionSchema.validate(req.body);
-    if (error) {
-      return res.status(400).json({
-        message: 'Missing required name field',
-      });
-    }
-
-    const newTransaction = await Transactions.add(req.body);
-    res.status(201).json({ newTransaction });
-  } catch (error) {
-    next(error);
-  }
+  const result = await Transactions.create(req.body);
+  res.status(201).json({ result });
 };
+
+// const transactionsOperations = require("../../model/transactions");
+
+// const { joiTransactionSchema } = require("../../validation");
+
+// const add = async (req, res, next) => {
+//   try {
+//     const { error } = joiTransactionSchema.validate(req.body);
+//     if (error) {
+//       return res.status(400).json({
+//         message: "Missing required name field",
+//       });
+//     }
+
+//     const newTransaction = await transactionsOperations.add(req.body);
+//     res.status(201).json({ newTransaction });
+//   } catch (error) {
+//     next(error);
+//   }
+// };
 
 module.exports = add;
