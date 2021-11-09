9a76453d71a3139dc6ced6f863269922a129fbdc
Volodymyr Viter <mad.tvido@gmail.com>
2021-11-09 13:03:14 +0200

    Update add.js

--- controllers/transactions/add.js
+++ controllers/transactions/add.js
@@ -1,22 +1,22 @@
 const { Transactions } = require('../../models/transactions');
 
 const add = async (req, res, next) => {
-  // const result = await Transactions.create(req.body);
-  // res.status(201).json({ result });
+  const result = await Transactions.create(req.body);
+  res.status(201).json({ result });
 
-  try {
-    const { error } = Transactions.validate(req.body);
-    if (error) {
-      return res.status(400).json({
-        message: 'Missing required name field',
-      });
-    }
+  // try {
+  //   const { error } = Transactions.validate(req.body);
+  //   if (error) {
+  //     return res.status(400).json({
+  //       message: 'Missing required name field',
+  //     });
+  //   }
 
-    const newTransaction = await Transactions.add(req.body);
-    res.status(201).json({ newTransaction });
-  } catch (error) {
-    next(error);
-  }
+  //   const newTransaction = await Transactions.add(req.body);
+  //   res.status(201).json({ newTransaction });
+  // } catch (error) {
+  //   next(error);
+  // }
 };
 
 // const transactionsOperations = require("../../model/transactions");
